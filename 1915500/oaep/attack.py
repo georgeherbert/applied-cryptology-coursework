ce73a5b32f2b944d085531d1aa319336ff7112c6
georgeherbert <george.herbert@icloud.com>
2022-02-17 14:47:31 +0000

    Added an int to pretty hex function that converts an integer to a hexadecimal number of the correct format

--- 1915500/oaep/attack.py
+++ 1915500/oaep/attack.py
@@ -2,22 +2,46 @@ import sys
 import subprocess
 
 TARGET = subprocess.Popen(
-    args = sys.argv[1],
+    args = f"./{sys.argv[1]}",
     stdin = subprocess.PIPE,
     stdout = subprocess.PIPE
 )
 TARGET_IN = TARGET.stdin
 TARGET_OUT = TARGET.stdout
 
-def interact():
-    TARGET_IN.write("10:0A774558EC7D0C748FCC53DBAEF82D85\n".encode())
-    TARGET_IN.write("80:2C1E28DDDBF06CD7F8EE16DA9D19079A92193A2EB54DBA01278CDEB17EB1A5D3DA2ECA6F6F217C48A17AFEC598B89AB655F3A4373609C6C6F0E1711DC7F0F23D5480F3B54C7EA6E69EB78ADEA6C74F93ED8BD40A7498D205258BD5FA71331ABCFA278CF4E3542183DEB27401FE93BDA75498C31587BA207595CFBE551ECC450B\n".encode())
+def get_attack_params():
+    with open(sys.argv[2], "r") as config:
+        return [config.readline().strip() for _ in range(4)]
+
+def int_to_pretty_hex(value_int):
+    value_hex = f"{value_int:x}"
+    value_hex_length = len(value_hex)
+    odd_length = value_hex_length % 2 == 1
+    if odd_length:
+        value_hex = "0" + value_hex
+    num_octets = value_hex_length // 2 + odd_length
+    return f"{num_octets:x}:{value_hex}"
+
+def interact(label, ciphertext):
+    TARGET_IN.write(f"{label}\n".encode())
+    TARGET_IN.write(f"{ciphertext}\n".encode())
     TARGET_IN.flush()
     return int(TARGET_OUT.readline().strip())
 
 def attack():
-    response = interact()
-    print(response)
+    modulus, public_exponent, label, ciphertext = get_attack_params()
+
+    modulus_int = int(modulus, 16)
+    exponent_int = int(public_exponent, 16)
+    ciphertext_int = int(ciphertext.split(":")[1], 16)
+
+    print(int_to_pretty_hex(1)) # 1:01
+    print(int_to_pretty_hex(17)) # 1:11
+    print(int_to_pretty_hex(255)) # 1:FF
+    print(int_to_pretty_hex(256)) # 2:0100
+
+    # response = interact(label, guess)
+    # print(response)
 
 if __name__ == "__main__":
     attack()
