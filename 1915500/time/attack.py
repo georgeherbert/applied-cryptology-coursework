ba2ab162741a6e644c8a9fa8d5550cc3dee7b974
georgeherbert <george.herbert@icloud.com>
2022-02-25 22:59:38 +0000

    Mongomery exponentiation seems to be working

--- 1915500/time/attack.py
+++ 1915500/time/attack.py
@@ -31,22 +31,39 @@ def mont_omega(n):
 def mont_rho_sq(n, l_n):
     return pow(2, 2 * l_n * W, n)
 
+def interact(ciphertext):
+    TARGET_IN.write(f"{ciphertext:x}\n".encode())
+    TARGET_IN.flush()
+    time = int(TARGET_OUT.readline().strip())
+    message = int(TARGET_OUT.readline().strip())
+    return time, message
+
 def mpz_getlimbn(op, n):
     return op >> (n * W) & (2 ** W - 1)
 
-def montmul(x, y, l_n, omega, n):
+def mont_mul(x, y, l_n, omega, n):
     r = 0
     subtraction = False
+    x_0 = mpz_getlimbn(x, 0)
     for i in range(0, l_n):
         r_0 = mpz_getlimbn(r, 0)
         y_i = mpz_getlimbn(y, i)
-        x_0 = mpz_getlimbn(x, i)
         u_i = ((r_0 + y_i * x_0) * omega) % B
-        r = (r + y_i + x + u_i * n) >> W
+        r = (r + y_i * x + u_i * n) >> W
     if r >= n:
         r -= n
         subtraction = True
     return r, subtraction
+
+def mont_exp(rho_sq, n, x, y, l_n, omega):
+    t = mont_mul(1, rho_sq, l_n, omega, n)[0]
+    x = mont_mul(x, rho_sq, l_n, omega, n)[0]
+    y_size = int(math.log(y, 2))
+    for i in range(y_size, -1, -1):
+        t = mont_mul(t, t, l_n, omega, n)[0]
+        if (y >> i) & 1:
+            t = mont_mul(t, x, l_n, omega, n)[0]
+    return mont_mul(t, 1, l_n, omega, n)[0]
 
 def attack():
     n, e = get_attack_params()
@@ -54,11 +71,12 @@ def attack():
     omega = mont_omega(n)
     rho_sq = mont_rho_sq(n, l_n)
 
-    print(omega)
-    print("")
-    print(rho_sq)
+    # ciphertext_samples = [random.randrange(0, n) for _ in range(1000)]
 
-    # ciphertext_samples = [random.randrange(0, n) for _ in range(100)]
+
+    print(mont_exp(rho_sq, n, 3, 5, l_n, omega))
+
+
 
 if __name__ == "__main__":
     attack()
