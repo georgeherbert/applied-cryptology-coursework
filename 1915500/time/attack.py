a85c22b70d6eaf359c0b968fbbbc47d459f09fbd
georgeherbert <george.herbert@icloud.com>
2022-02-25 20:58:50 +0000

    Started to implement montgomery functions

--- 1915500/time/attack.py
+++ 1915500/time/attack.py
@@ -2,6 +2,7 @@ import sys
 import subprocess
 import random
 import math
+from tkinter import N
 
 TARGET = subprocess.Popen(
     args = f"./{sys.argv[1]}",
@@ -18,8 +19,8 @@ def get_attack_params():
     with open(sys.argv[2], "r") as config:
         return [int(config.readline().strip(), 16) for _ in range(2)]
 
-def mpz_size(n):
-    return math.ceil(math.log(n, B))
+def mpz_size(op):
+    return math.ceil(math.log(op, B))
 
 def mont_omega(n):
     t = 1
@@ -29,6 +30,23 @@ def mont_omega(n):
     
 def mont_rho_sq(n, l_n):
     return pow(2, 2 * l_n * W, n)
+
+def mpz_getlimbn(op, n):
+    return op >> (n * W) & (2 ** W - 1)
+
+def montmul(x, y, l_n, omega, n):
+    r = 0
+    subtraction = False
+    for i in range(0, l_n):
+        r_0 = mpz_getlimbn(r, 0)
+        y_i = mpz_getlimbn(y, i)
+        x_0 = mpz_getlimbn(x, i)
+        u_i = ((r_0 + y_i * x_0) * omega) % B
+        r = (r + y_i + x + u_i * n) >> W
+    if r >= n:
+        r -= n
+        subtraction = True
+    return r, subtraction
 
 def attack():
     n, e = get_attack_params()
@@ -40,8 +58,7 @@ def attack():
     print("")
     print(rho_sq)
 
-    # random_messages = [random.randint(0, n - 1) for _ in range(50)]
-    # print(random_messages)
+    # ciphertext_samples = [random.randrange(0, n) for _ in range(100)]
 
 if __name__ == "__main__":
     attack()
