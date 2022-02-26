d9be34492a873d8b54a132f2990c3de4876e8ac8
georgeherbert <george.herbert@icloud.com>
2022-02-26 16:00:04 +0000

    Think I'm starting to get somewhere with the difference checking

--- 1915500/time/attack.py
+++ 1915500/time/attack.py
@@ -2,7 +2,6 @@ import sys
 import subprocess
 import random
 import math
-from tkinter import N
 
 TARGET = subprocess.Popen(
     args = f"./{sys.argv[1]}",
@@ -35,7 +34,7 @@ def interact(ciphertext):
     TARGET_IN.write(f"{ciphertext:x}\n".encode())
     TARGET_IN.flush()
     time = int(TARGET_OUT.readline().strip())
-    message = int(TARGET_OUT.readline().strip())
+    message = int(TARGET_OUT.readline().strip(), 16)
     return time, message
 
 def mpz_getlimbn(op, n):
@@ -65,18 +64,50 @@ def mont_exp(rho_sq, n, x, y, l_n, omega):
             t = mont_mul(t, x, l_n, omega, n)[0]
     return mont_mul(t, 1, l_n, omega, n)[0]
 
+def square_and_multiply_init(rho_sq, l_n, omega, n, x):
+    # Conversion of values at the start of mont_exp
+    t = mont_mul(1, rho_sq, l_n, omega, n)[0]
+    # First loop round since assuming first bit is 1
+    t = mont_mul(t, t, l_n, omega, n)[0]
+    t = mont_mul(t, x, l_n, omega, n)[0] # Assumes first bit is 1
+    # Performs last calculation before checking the bit of the second iteration
+    t = mont_mul(t, t, l_n, omega, n)[0]
+    return t
+
+def square_and_multiply_next(d_i, t, x, l_n, omega, n):
+    if d_i:
+        t = mont_mul(t, x, l_n, omega, n)[0]
+    t = mont_mul(t, t, l_n, omega, n)
+    return t
+
 def attack():
     n, e = get_attack_params()
     l_n = mpz_size(n)
     omega = mont_omega(n)
     rho_sq = mont_rho_sq(n, l_n)
+    print(f"n: {n}\n\ne: {e}\n\nl_n: {l_n}\n\nomega: {omega}\n\nrho_sq: {rho_sq}\n\n")
 
-    # ciphertext_samples = [random.randrange(0, n) for _ in range(1000)]
+    ciphertext_samples = [random.randrange(0, n) for _ in range(50000)]
+    ciphertext_times = [interact(ciphertext)[0] for ciphertext in ciphertext_samples]
+    ciphertext_monts = [mont_mul(c, rho_sq, l_n, omega, n)[0] for c in ciphertext_samples] ## IMPORTANT!
 
+    m_temps = [square_and_multiply_init(rho_sq, l_n, omega, n, x) for x in ciphertext_monts]
+    
+    ciphertext_mont_bit_one = [square_and_multiply_next(True, m_temp, x, l_n, omega, n) for m_temp, x in zip(m_temps, ciphertext_monts)]
+    ciphertext_mont_bit_zero = [square_and_multiply_next(False, m_temp, x, l_n, omega, n) for m_temp, x in zip(m_temps, ciphertext_monts)] 
 
-    print(mont_exp(rho_sq, n, 1203, 300, l_n, omega))
+    M_1 = [ciphertext_times[i] for i, (_, reduction) in enumerate(ciphertext_mont_bit_one) if reduction]
+    M_2 = [ciphertext_times[i] for i, (_, reduction) in enumerate(ciphertext_mont_bit_one) if not reduction]
+    M_3 = [ciphertext_times[i] for i, (_, reduction) in enumerate(ciphertext_mont_bit_zero) if reduction]
+    M_4 = [ciphertext_times[i] for i, (_, reduction) in enumerate(ciphertext_mont_bit_zero) if not reduction]
 
+    M_1_mean = sum(M_1) / len(M_1)
+    M_2_mean = sum(M_2) / len(M_2)
+    M_3_mean = sum(M_3) / len(M_3)
+    M_4_mean = sum(M_4) / len(M_4)
 
+    print(abs(M_1_mean - M_2_mean))
+    print(abs(M_3_mean - M_4_mean))
 
 if __name__ == "__main__":
     attack()
