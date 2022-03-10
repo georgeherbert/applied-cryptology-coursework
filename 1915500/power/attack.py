844cab989ef93b02d677f97a55883ea4bb9fc947
georgeherbert <george.herbert@icloud.com>
2022-03-10 22:28:19 +0000

    Power consumption prints

--- 1915500/power/attack.py
+++ 1915500/power/attack.py
@@ -9,8 +9,18 @@ TARGET = subprocess.Popen(
 TARGET_IN = TARGET.stdin
 TARGET_OUT = TARGET.stdout
 
+def interact(j, i):
+    TARGET_IN.write(f"{j}\n".encode())
+    TARGET_IN.write(f"10:{i:0{16 * 2}x}\n".encode())
+    TARGET_IN.flush()
+    power_consumption = TARGET_OUT.readline().strip()
+    m = TARGET_OUT.readline().strip()
+    return power_consumption, m
+
 def attack():
-    pass
+    power_consumption, m = interact(10, 100)
+    print(power_consumption)
+    print(m)
 
 if  __name__ == "__main__":
     attack()
